d60821dc685d216b334b6d7ab92b5277b7b97cf9
1698-Erika <erikart1698@gmail.com>
2023-07-24 21:33:01 -0600

    Ejemplos del uso de array y objetos con ciclo for

---
+++ Bucle For/main.js
@@ -0,0 +1,42 @@
+//console.log('Hola Mundo')
+ let suma = 0;
+for(
+    //inicializador
+ let inicializador = 1; 
+    //condicion 
+   inicializador <= 20; 
+    //actualizacion de la variable de inicial
+   inicializador ++
+    )
+   if(inicializador %2 === 0) {
+   suma += inicializador
+}
+    {
+   console.log( suma )
+}
+
+
+let persona = { 
+    nombre : 'Lucero',
+    edad : 35, 
+    direccion : 'Calle'
+}
+
+let animalesArray = ['gato','perro','delfin', 5 ,'Lucero',  3, persona]
+for(
+    //inicializador
+    let i = 0; 
+    //condicion 
+    i <= animalesArray.lenght; 
+    //actualizacion de la variable de inicial
+    i ++ )
+    {
+    console.log(animalesArray[1])
+}
+
+
+   
+
+
+
+
