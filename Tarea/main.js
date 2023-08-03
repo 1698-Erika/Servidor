bc34045884a2bd7ddabc6760a2cc8a90834fc90b
1698-Erika <erikart1698@gmail.com>
2023-08-02 21:12:33 -0600

    Ejercicio de un array con objetos

---
+++ Tarea/main.js
@@ -0,0 +1,66 @@
+const tareas = [
+    {
+        titulo: "Completar proyecto",
+        descripcion: "Finalizar la implementación del proyecto.",
+        prioridad: "alta",
+        completada: false,
+        actividades: [
+            {
+                nombre: 'ejemplo',
+                calificacion: 10
+            },
+        ]
+    },
+    {
+        titulo: "Enviar informe",
+        descripcion: "Enviar el informe de resultados al cliente.",
+        prioridad: "media",
+        completada: true,
+        actividades: [
+            {
+                nombre: 'Lectura',
+                calificacion: 10
+            },
+        ]
+    },
+    {
+        titulo: "Reunión de equipo",
+        descripcion: "Asistir a la reunión de planificación.",
+        prioridad: "baja",
+        completada: false,
+        actividades: [
+            {
+                nombre: 'ejemplo',
+                calificacion: 10
+            },
+        ]
+    },
+    {
+        titulo: "Actualizar sitio web",
+        descripcion: "Realizar cambios y actualizaciones en el sitio web.",
+        prioridad: "alta",
+        completada: true,
+        actividades: [
+            {
+                nombre: 'ejemplo',
+                calificacion: 10
+            },
+            {
+                nombre: 'ejemplo2',
+                calificacion: 10
+            },
+        ]
+    },
+    {
+        titulo: "Investigar nuevas tecnologías",
+        descripcion: "Investigar y evaluar nuevas tecnologías para el proyecto.",
+        prioridad: "media",
+        completada: false,
+        actividades: [
+            {
+                nombre: 'ejemplo',
+                calificacion: 10
+            },
+        ]
+    }
+];
