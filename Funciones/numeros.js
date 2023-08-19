b35003f9db63078c59e4cc8003fec0606540e0c0
1698-Erika <erikart1698@gmail.com>
2023-08-18 22:01:33 -0600

    Muestra de numero y suma, con funciones

---
+++ Funciones/numeros.js
@@ -0,0 +1,27 @@
+function validarNumero(num) {
+    if (num > 50) {
+      console.log(" El numero es mayor a 50");
+    } else {
+      console.log("El numero es menor a 50");
+    }
+  }
+  const numeroUno = parseInt(prompt("Ingresa el numero "));
+  validarNumero(numeroUno);
+  //const numeroDos = parseInt(prompt("Ingresa el numero "));
+  //validarNumero(numeroDos);
+  
+  const ejemploNumero = 72
+  validarNumero (ejemploNumero);
+  
+  validarNumero(4);
+
+  
+function sumaNumeros(numeroUno, numeroDos) {
+
+let resultado = numeroUno + numeroDos
+    console.log(resultado)
+
+}
+
+sumaNumeros( 2 , 3)
+
