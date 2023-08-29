63b6b34f032b5785550e3006aab623edabef3715
1698-Erika <erikart1698@gmail.com>
2023-08-28 22:30:26 -0600

    Creacion de suma en proyecto Calculadora

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,36 +1,40 @@
-let evaluacion;
-
 function obtenerNumeros() {
   let numeroUno = parseInt(prompt("Ingresa el numero "));
   let numeroDos = parseInt(prompt("Ingresa el numero "));
-  return {numeroUno, numeroDos}
+  return { numeroUno, numeroDos };
 }
 
-while (evaluacion !== 4) {
- // evaluacion = prompt("Ingresa el numero evalucion ");
+function suma() {
+  const numeros = obtenerNumeros();
 
+  let resultado1 = numeros.numeroUno + numeros.numeroDos;
+  console.log(resultado1);
+}
+
+function resta(numeroUno, numeroDos) {
+  const numeros = obtenerNumeros();
+  let resultado2 = numeroUno - numeroDos;
+  console.log(resultado2);
+}
+
+function multiplicacion(numeroUno, numeroDos) {
+  let resultado3 = numeroUno * numeroDos;
+  console.log(resultado3);
+}
+
+let evaluacion = 0 ;
+while (evaluacion < 4) {
   console.log("---Evaluaciones ---");
-  console.log("1:" + evaluacion);
-  console.log("2:" + evaluacion);
-  console.log("3:" + evaluacion);
-  console.log("4: Salir");
+  console.log("1 suma ");
+  console.log("2 resta");
+  console.log("3 multiplicacion ");
+  console.log("4 Salir");
 
- const ejemplo = obtenerNumeros();
- console.log(ejemplo)
+  evaluacion = prompt("Ingresa el numero evalucion ");
 
 
-//   function suma(numeroUno, numeroDos) {
-//     let resultado = numeroUno + numeroDos;
-//     console.log(resultado);
-//   }
+  suma()
 
-//   function resta(numeroUno, numeroDos) {
-//     let resultado = numeroUno - numeroDos;
-//     console.log(resultado);
-//   }
-
-//   function multiplicacion(numeroUno, numeroDos) {
-//     let resultado = numeroUno * numeroDos;
-//     console.log(resultado);
-//   }
+  //function obtenerNumeros() {
+  //return {resultado}
 }
