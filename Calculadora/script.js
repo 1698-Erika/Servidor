4f82f14cb101611359f473d7d8455b58bf1b3b24
1698-Erika <erikart1698@gmail.com>
2023-08-30 13:41:19 -0600

    Identificacion de cada funcion desde la seleccion de usuario

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -8,32 +8,50 @@ function suma() {
   const numeros = obtenerNumeros();
 
   let resultado1 = numeros.numeroUno + numeros.numeroDos;
-  console.log(resultado1);
+  return resultado1
 }
 
-function resta(numeroUno, numeroDos) {
+
+function resta() {
   const numeros = obtenerNumeros();
-  let resultado2 = numeroUno - numeroDos;
+
+  let resultado2 = numeros.numeroUno - numeros.numeroDos;
   console.log(resultado2);
 }
 
 function multiplicacion(numeroUno, numeroDos) {
-  let resultado3 = numeroUno * numeroDos;
+  const numeros = obtenerNumeros();
+
+  let resultado3 = numeros.numeroUno * numeros.numeroDos;
   console.log(resultado3);
 }
 
-let evaluacion = 0 ;
-while (evaluacion < 4) {
+let seleccion = 0;
+while (seleccion < 4) {
   console.log("---Evaluaciones ---");
   console.log("1 suma ");
   console.log("2 resta");
   console.log("3 multiplicacion ");
   console.log("4 Salir");
 
-  evaluacion = prompt("Ingresa el numero evalucion ");
+  seleccion = parseFloat (prompt("Ingresa el numero evaluacion "));
 
+  if( seleccion === 1){
+    const resultado = suma()
+    console.log("El resultado es : " +resultado)
+  }
 
-  suma()
+else if (seleccion === 2){
+   const resultado = resta()
+
+  console.log("El resultado es : " +resultado)
+}
+else if (seleccion === 3){
+  const resultado = multiplicacion()
+
+  console.log("El resultado es : " +resultado )
+}
+  
 
   //function obtenerNumeros() {
   //return {resultado}
