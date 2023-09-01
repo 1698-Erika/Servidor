a41956620d4a04d90c203028b1330c729da6b6f0
1698-Erika <erikart1698@gmail.com>
2023-08-31 21:24:41 -0600

    Termino de Calculadora con tres tipos de obtener resultados

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -8,25 +8,25 @@ function suma() {
   const numeros = obtenerNumeros();
 
   let resultado1 = numeros.numeroUno + numeros.numeroDos;
-  return resultado1
+  return resultado1;
 }
-
 
 function resta() {
   const numeros = obtenerNumeros();
 
   let resultado2 = numeros.numeroUno - numeros.numeroDos;
-  console.log(resultado2);
+  console.log("El resultado de la resta es: " + resultado2);
 }
 
 function multiplicacion(numeroUno, numeroDos) {
   const numeros = obtenerNumeros();
-
-  let resultado3 = numeros.numeroUno * numeros.numeroDos;
-  console.log(resultado3);
+  return numeros.numeroUno * numeros.numeroDos;
+  //console.log(resultado3);
+  //return resultado3;
 }
 
 let seleccion = 0;
+
 while (seleccion < 4) {
   console.log("---Evaluaciones ---");
   console.log("1 suma ");
@@ -34,25 +34,18 @@ while (seleccion < 4) {
   console.log("3 multiplicacion ");
   console.log("4 Salir");
 
-  seleccion = parseFloat (prompt("Ingresa el numero evaluacion "));
-
-  if( seleccion === 1){
-    const resultado = suma()
-    console.log("El resultado es : " +resultado)
+  seleccion = parseFloat(prompt("Ingresa el numero evaluacion "));
+  if (seleccion === 1) {
+    const resultado = suma();
+    console.log("El resultado es : " + resultado);
+  } else if (seleccion === 2) {
+    const resultado = resta();
+    //console.log("El resultado es : " + resultado);
+  } else if (seleccion === 3) {
+    console.log("El resultado es : ", multiplicacion());
+  } else if (seleccion === 4) {
+    console.log("No se selecciono formula ");
   }
-
-else if (seleccion === 2){
-   const resultado = resta()
-
-  console.log("El resultado es : " +resultado)
 }
-else if (seleccion === 3){
-  const resultado = multiplicacion()
-
-  console.log("El resultado es : " +resultado )
-}
-  
-
-  //function obtenerNumeros() {
-  //return {resultado}
-}
+//function obtenerNumeros() {
+//return {resultado}
