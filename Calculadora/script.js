784030a33e843c799ea9da777ee30949bc5cb941
1698-Erika <erikart1698@gmail.com>
2023-09-22 02:56:38 -0600

    Correccion de resta

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,11 +1,11 @@
 let seleccion = 0;
 let total;
 
-
 function numeroFinal() {
   let numeroDos = parseInt(prompt("Ingresa el numero "));
   return numeroDos;
 }
+
 function obtenerNumeros() {
   let numeroUno = parseInt(prompt("Ingresa el numero "));
   let numeroDos = parseInt(prompt("Ingresa el numero "));
@@ -15,23 +15,21 @@ function obtenerNumeros() {
 function suma() {
   if (!total) {
     const numeros = obtenerNumeros();
-     total = numeros.numeroUno + numeros.numeroDos;
+    total = numeros.numeroUno + numeros.numeroDos;
     console.log("El resultado de la suma es: " + total);
   } else {
     const nuevoNumero = numeroFinal();
-    total = total + nuevoNumero
+    total = total + nuevoNumero;
     console.log("El resultado de la suma es: " + total);
   }
 }
 
 function resta() {
-
   const numeros = obtenerNumeros();
   let resultado2 = numeros.numeroUno - numeros.numeroDos;
   console.log("El resultado de la resta es: " + resultado2);
 
-  siguienteOperacion(resultado2)
-
+  siguienteOperacion(resultado2);
 }
 
 function multiplicacion(numeroUno, numeroDos) {
@@ -40,47 +38,40 @@ function multiplicacion(numeroUno, numeroDos) {
   //console.log(resultado3);
   //return resultado3;
 }
-
 function mostrarOperaciones() {
   console.log("1 suma ");
   console.log("2 resta");
   console.log("3 multiplicacion ");
+  console.log("4 Salir");
 }
-
 function siguienteOperacion(x) {
-
   console.log("Cual es la siguiente operacion que desea realizar");
 
   mostrarOperaciones();
   mostrarSeleccion();
 
   const nuevoNumero = numeroFinal();
-  const res = x - nuevoNumero
-  console.log("El resultado es: " +res);
+  const res = x - nuevoNumero;
+  console.log("El resultado es: " + res);
 
-  siguienteOperacion()
+  siguienteOperacion(res);
 }
 
 function mostrarSeleccion() {
   seleccion = parseFloat(prompt("Ingresa el numero evaluacion "));
   return seleccion;
 }
-
-function operacionFinal() {
-  if (seleccion === 1) console.log(suma());
-  else if (seleccion === 2) console.log(resta());
-  else if (seleccion === 2) console.log(multiplicacion());
+ function mosrearEcuaciones()
+{
+  if 
 }
-
 while (seleccion < 4) {
   console.log("---Evaluaciones ---");
   mostrarOperaciones();
-  console.log("4 Salir");  
-  mostrarSeleccion()
-
+  mostrarSeleccion();
 
   if (seleccion === 1) {
-     suma()
+    suma();
     //console.log("El resultado es : " + resultado);
   } else if (seleccion === 2) {
     const resultado = resta();
@@ -91,9 +82,7 @@ while (seleccion < 4) {
     console.log("No se selecciono formula ");
   }
 
-
-
-//  operacionFinal();
+  //  operacionFinal();
 }
 //function obtenerNumeros() {
 //return {resultado}
